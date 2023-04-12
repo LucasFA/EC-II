fd337dffcf7aefbc53d4af04fc2237a17b298b23
LucasFA <lucaslanzarote@gmail.com>
2023-04-12 22:46:33 +0200

    Reordered stuff

--- actividades/actividad 2.r
+++ actividades/actividad 2.r
@@ -1,5 +1,12 @@
-is_real_number <- function(vector) {
-    expr <- paste0(
+
+test <- c(
+    "hola", "1", " adios", "8.25", "-148.678",
+    "52  ", " 34", "48", "3.45.56", "a3.5",
+    "3.5a4", "8.", "-234", "00.45", "38b", "3.00", "0h3", "0.5", "- 4"
+)
+salida_test <- c("1", "8.25", "-148.678", "48", "-234", "3.00", "0.5")
+
+expr <- paste0(
         "^",
         "([+-]?)",
         "(",
@@ -9,16 +16,12 @@ is_real_number <- function(vector) {
         ")",
         "(\\.\\d+)?",
         "$"
-    )
-    print(expr)
+)
+print(expr)
+
+is_real_number <- function(vector) {
     vector[grepl(expr, vector)]
 }
-test <- c(
-    "hola", "1", " adios", "8.25", "-148.678",
-    "52  ", " 34", "48", "3.45.56", "a3.5",
-    "3.5a4", "8.", "-234", "00.45", "38b", "3.00", "0h3", "0.5", "- 4"
-)
-salida_test <- c("1", "8.25", "-148.678", "48", "-234", "3.00", "0.5")
 
 is_real_number(test)
 salida_test
